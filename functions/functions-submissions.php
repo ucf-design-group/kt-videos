2c997b0a1065fdbb005ed3b911bfae3730b66315
John Costallos <jchristopher@knights.ucf.edu>
2014-02-04 17:44:49 -0500

    Create Vote Handler

--- functions/functions-submissions.php
+++ functions/functions-submissions.php
@@ -32,6 +32,7 @@ function kt_submission_meta() {
 	$visible = get_post_meta($post->ID, 'kt-form-visible', true) ? get_post_meta($post->ID, 'kt-form-visible', true) : 'show';
 	$order = get_post_meta($post->ID, 'kt-form-order', true) ? get_post_meta($post->ID, 'kt-form-order', true) : '0';
 	$video = get_post_meta($post->ID, 'kt-form-video', true) ? get_post_meta($post->ID, 'kt-form-video', true) : '';
+	$url = get_post_meta($post->ID, 'kt-form-url', true) ? get_post_meta($post->ID, 'kt-form-url', true) : '';
 
 	// Make special arrangements for possibly disabling the end date/time fields
 
@@ -46,26 +47,20 @@ function kt_submission_meta() {
 	?>
 	<style type="text/css">
 		#kt-form-main {width: 100%}
-		#kt-form-order, #kt-form-video {width: 80px;}
+		#kt-form-order, #kt-form-video, #kt-form-url {width: 80px;}
 	</style>
 	<script type="text/javascript">
 		function visibleCheckBox() {
 			var checkBox = document.getElementById('kt-form-visible');
 			var orderInput = document.getElementById('kt-form-order');
-			var videoInput = document.getElementById('kt-form-video');
+
 			if (checkBox.checked) {
 				orderInput.disabled = false;
 				orderInput.style.backgroundColor = '#FFFFFF';
-				
-				videoInput.disabled = false;
-				videoInput.style.backgroundColor = '#FFFFFF';
 			}
 			else {
 				orderInput.disabled = true;
 				orderInput.style.backgroundColor = '#EEEEEE';
-				
-				videoInput.disabled = true;
-				videoInput.style.backgroundColor = '#EEEEEE';
 			}
 		}
 	</script>
@@ -78,7 +73,10 @@ function kt_submission_meta() {
 			<td><input type="text" name="kt-form-order" id="kt-form-order" value="<?php echo $order; ?>" <?php echo $disabled . " " . $style; ?>></td>
 		</tr><tr>
 			<th><label for="kt-form-video">YouTube ID:</label></th>
-			<td><input type="text" name="kt-form-video" id="kt-form-video" value="<?php echo $video; ?>" <?php echo $disabled . " " . $style; ?>></td>
+			<td><input type="text" name="kt-form-video" id="kt-form-video" value="<?php echo $video; ?>"></td>
+		</tr><tr>
+			<th><label for="kt-form-url">Team URL:</label></th>
+			<td><input type="text" name="kt-form-url" id="kt-form-url" value="<?php echo $url; ?>"></td>
 		</tr>
 	</table>
 	<?php
@@ -104,7 +102,8 @@ function submission_meta_save($post_id, $post) {
 
 	$input['visible'] = (isset($_POST['kt-form-visible']) && $_POST['kt-form-visible'] == 'show' ? 'show' : 'hide');
 	$input['order'] = (isset($_POST['kt-form-order']) ? $_POST['kt-form-order'] : '0');
-	$input['video'] = (isset($_POST['kt-form-video']) ? $_POST['kt-form-video'] : get_the_title());
+	$input['video'] = (isset($_POST['kt-form-video']) ? $_POST['kt-form-video'] : '');
+	$input['url'] = (isset($_POST['kt-form-url']) ? $_POST['kt-form-url'] : '');
 
 	foreach ($input as $field => $value) {
 
---
+++ functions/vote-handler.php
@@ -0,0 +1,48 @@
+<?php
+
+$message = null;
+
+if (isset($_POST['vote-form-submit']))
+	$message = count_votes();
+
+
+
+function count_votes () {
+
+	if ($_POST['vote-form-email'] == "")
+		return "Please provide your e-mail address.";
+
+	if (!filter_var($_POST['vote-form-email'], FILTER_VALIDATE_EMAIL))
+		return "Please provide a valid e-mail address.";
+
+	if (!isset($_POST['vote-form-video']))
+		return "You didn't vote!";
+
+
+	$db = new mysqli("localhost", "osi-admin", "Gold&Black", "kt-votes");
+
+	$check = $db->query("SELECT * FROM `emails` WHERE email='" . $_POST['vote-form-email'] . "';");
+
+	if ($check->num_rows > 0)
+		return "You've already voted!";
+
+
+	$videoID = mysql_real_escape_string($_POST['vote-form-video']);
+
+
+	$old = get_post_meta(intval($videoID), 'kt-form-votes', true);
+
+	$new = intval($old) + 1;
+
+	if ($new && '' == $old)
+		add_post_meta(intval($videoID), 'kt-form-votes', $new, true);
+	else if ($new && $new != $old)
+		update_post_meta(intval($videoID), 'kt-form-votes', $new);
+
+
+	$db->query("INSERT INTO `emails` VALUES ('" . $_POST['vote-form-email'] . "');");
+
+	return "Thank you!  Your vote has been cast.";
+}
+
+?>
