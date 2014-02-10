24ec6d8ab9c490b0bd8e996e0af44b8ab95f25cc
John Costallos <jpcostallos@gmail.com>
2014-02-10 12:45:35 -0500

    Add Mobile styles and move counter to outside of content area

--- home.php
+++ home.php
@@ -10,14 +10,14 @@ get_header(); ?>
 						get_template_part( 'content', 'page' );
 					} ?>
 				</div>
-					<!-- Complete credit for the beautiful timer: http://codepen.io/mel/details/nleBw -->
-					<div class="countdown-timer">
-						<div id="days"></div>
-						<div id="hours"></div>
-						<div id="minutes"></div>
-						<div id="seconds"></div>
-						<h1>Until Knight-Thon</h1>
-					</div>
-			</div>
 
+			</div>
+			<!-- Complete credit for the beautiful timer: http://codepen.io/mel/details/nleBw -->
+			<div class="countdown-timer">
+				<div id="days"></div>
+				<div id="hours"></div>
+				<div id="minutes"></div>
+				<div id="seconds"></div>
+				<h1>Until Knight-Thon</h1>
+			</div>
 <?php get_footer(); ?>
